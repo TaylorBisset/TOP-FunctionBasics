4b06a40cfd297a6247d8c2ce3589d01425a57528
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-13 16:57:46 -0700

    lastLetter

--- javascript.js
+++ javascript.js
@@ -33,3 +33,13 @@ function capitalize(word)
 console.log(capitalize("lowercase"));
 console.log(capitalize("UPPERCASE"));
 console.log(capitalize("BoTh"));
+
+// Write a function called lastLetter that takes a string 
+// and returns the very last letter of that string:
+// lastLetter("abcd") should return "d"
+
+function lastLetter(words)
+{
+    return words.slice(-1);
+}
+console.log(lastLetter("abcd"));
