190c8f87549e0048e96bc814cd7e3a720e500680
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-13 16:45:33 -0700

    add7

--- javascript.js
+++ javascript.js
@@ -7,5 +7,9 @@ function favoriteAnimal(animal)
 
 console.log(favoriteAnimal('Goat'))
 
-console.log("- - - - - - - - - -")
-
+// Write a function called add7 that takes one number and returns that number + 7.
+function add7(number)
+{
+    return number + 7;
+}
+console.log(add7(5));
