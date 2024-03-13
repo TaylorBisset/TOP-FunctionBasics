d16502f29f4c86f21c319f8819c62b727f3359e8
Taylor Bisset <87632154+TaylorBisset@users.noreply.github.com>
2024-03-13 16:55:18 -0700

    capitalize

--- javascript.js
+++ javascript.js
@@ -20,3 +20,16 @@ function multiply(a, b)
     return a * b;
 }
 console.log(multiply(4, 5));
+
+// Write a function called capitalize that takes a string 
+// and returns that string with only the first letter capitalized. 
+// Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
+
+function capitalize(word)
+{
+    word = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+console.log(capitalize("lowercase"));
+console.log(capitalize("UPPERCASE"));
+console.log(capitalize("BoTh"));
